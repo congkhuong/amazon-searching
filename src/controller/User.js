54f8ef6538143db4dee0fb38c9fc099a9d47d543
congkhuong <congkhuong1308@gmail.com>
2018-08-20 17:53:23 +0700

    s

--- src/controller/User.js
+++ src/controller/User.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 import MerchantAPI from '../lib/MerchantAPI';
+import * as CSV from 'csv-string';
+
 
 var request = require('request');
 
@@ -9,13 +11,15 @@ const test = async (req, res) => {
   /*const a = getPageContent(`https://www.amazon.com/dp/B003O7ZON4`);
   const b = getPageContent(`https://www.amazon.com/dp/B003O7ZON4`);*/
   //const url = 'https://www.amazon.com/dp/B003O7ZON4';
-  const url = 'https://www.merchantwords.com//search/us/rubik/sort-highest.csv';
 
   const merchantAPI = new MerchantAPI();
-  await merchantAPI.login();
-  const a = await merchantAPI.getEstSearchVolumn('rubiks');
-  logger.info(a.data);
+  await merchantAPI.login({ email: 'hiepkk3@gmail.com', 'password': 'hoanghiep9x' });
+  const a = await merchantAPI.getEstSearchVolumn('glass');
 
+  let merchantwordArr = CSV.parse(a.data);
+
+  merchantwordArr = merchantwordArr.slice(1, 21);
+  logger.info(merchantwordArr);
   //return request.get('https://www.merchantwords.com//search/us/rubik/sort-highest.csv');
 
   /*request.get('https://www.merchantwords.com//search/us/rubik/sort-highest.csv', function (error, response, body) {
@@ -30,12 +34,17 @@ const test = async (req, res) => {
     $ = cheerio.load(data.data);
     const t = $('.section__head-content h1').text();
   })*/
-
-  /*await getPageContent(url).then(data => {
-    logger.info(data.data);
-    $ = cheerio.load(data.data);
-    const t = $('.section__head-content h1').text();
-  })*/
+  //const aryKw = ['rubiks', 'rubik', 'glass'];
+  await Promise.all(merchantwordArr.map(async (merchantword) => {
+    const kw = merchantword[0];
+    const url = `https://www.amazon.com/s?field-keywords=${kw}`;
+    await getPageContent(url).then(data => {
+      $ = cheerio.load(data.data);
+      const t = $('#s-result-count').text();
+      logger.info(t);
+    })
+  }));
+  
   
   res.json({ data: 1 });
 };
